19211cf1cb54bd340002f633689e26dc2300adef
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 19:03:18 -0600

    Se modifica archivo Publication

--- src/components/publications/Publications.jsx
+++ src/components/publications/Publications.jsx
@@ -1,8 +1,10 @@
 import { Flex, Heading, Stack } from "@chakra-ui/react"
 import { CardPublication } from "./CardPublication"
 import { useContextPubication } from "../../shared/hooks/useContextPublication"
+import { useNavigate } from "react-router-dom"
 
 export const Publications = () => {
+    let navigate = useNavigate()
 
     const {publications} = useContextPubication()
     return (
@@ -12,9 +14,9 @@ export const Publications = () => {
                     <Heading>Publicaciones</Heading>
                 </div>
                 {console.log('Publications',publications)}
-            {publications.map((publi)=>(<CardPublication key={publi._id} 
+                {publications.map((publi)=>(<CardPublication key={publi._id}
                 title={publi.title} description={publi.description} course={publi.course.name}
-                proyect={publi.proyect} creationDate={publi.creationDate}/>))}
+                proyect={publi.proyect} creationDate={publi.creationDate} HandleClick={()=>{navigate(`/comments/${publi._id}`)}}/>))}
             </Stack>
         </Flex>
     )
