369ea7ebcff082c3582a4a6d0593e4bc2e1832a6
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 19:06:35 -0600

    Se modifica custom hook useCommentsByPublication

--- src/shared/hooks/useCommentsByPublication.jsx
+++ src/shared/hooks/useCommentsByPublication.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+import { getCommentsByPublicationIdRequest } from "../../services/api"
+import toast from "react-hot-toast"
 
 export const useCommentsByPublication = () => {
-    
-    return {
+    const [comments, setComments] = useState(null)
 
+    const getCommentsByPublication = async (id) => {
+        const response = await getCommentsByPublicationIdRequest(id)
+        if(response.error){
+            return toast.error(
+                response?.err?.response?.message ||
+                'Error al obtener los comentarios, por favor intente más tarde'
+            )
+        }
+        setComments(response.data.comments)
+    }
+    return {
+        comments,
+        isFetching: !comments,
+        getCommentsByPublication,
     }
 }
