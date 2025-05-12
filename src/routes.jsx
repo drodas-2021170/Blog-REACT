0e74c956d0fa4746beac5e119503966e6e049386
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 19:04:24 -0600

    Se agrega nueva ruta al proyecto

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
+import { CommentsPage } from "./pages/CommentsPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { Publication } from "./pages/Publication";
 
 export const routes = [
     {path:'/', element:<Publication/>},
+    {path:'/comments/:id', element:<CommentsPage/>},
     {path:'*', element:<NotFoundPage/>},
 ]
