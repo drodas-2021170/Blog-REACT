60620bc90f2b5f58445df3a980e5dc956ff54d21
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 18:56:49 -0600

    Se agrega archivo PublicationContent

--- src/components/PublicationContent.jsx
+++ src/components/PublicationContent.jsx
@@ -2,16 +2,33 @@
 import { useContextPubication } from '../shared/hooks/useContextPublication'
 import { FadeLoader } from 'react-spinners'
 import { Publications } from './publications/Publications'
+import { FilterBar } from './FilterBar'
+import { useState } from 'react'
 
 export const PublicationContent = () => {
-    const {isFetching} = useContextPubication()
-
+    const {publications,isFetching} = useContextPubication()
+    const [filter, setFilter] = useState('all')
     if(isFetching){
         return(
             <FadeLoader color='green'/>
         )
     }
+
+    const courses = publications.map((publication) => (publication.course.name))
+
+    const filteredCourses = courses.filter(course =>
+        filter === 'all' ? true : courses === filter
+    )
+
     return (
-        <Publications/>
+        <>
+            <FilterBar filter={filter} setFilter={setFilter}/>
+            {
+                filteredCourses.map((course)=>(
+                    <Publications />
+                )
+                )
+            }
+        </>
     )
 }
