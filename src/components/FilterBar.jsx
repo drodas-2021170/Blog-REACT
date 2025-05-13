b22d16a8353acacf72d13f335741f5a735873b6a
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-12 20:52:59 -0600

    Se modifica componente FilterBar

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -2,13 +2,21 @@ import { Select, HStack } from '@chakra-ui/react'
 
 export const FilterBar = ({ filter, setFilter }) => {
     return (
-        <HStack mb={4} spacing={4}>
-        <Select value={filter} onChange={e => setFilter(e.target.value)} maxW='200px'>
-            <option value='all'>Todos los cursos</option>
-            <option value='taller'>Taller</option>
-            <option value='tecnologia'>Tecnologia</option>
-            <option value='tics'>TICS</option>
-        </Select>
+        <HStack mb={4} spacing={4} margin={4}>
+            <Select 
+                value={filter} 
+                onChange={e => setFilter(e.target.value)} 
+                maxW="200px" 
+                borderColor="#FF6F61" 
+                bg="#FFFAE5"
+                _hover={{ bg: "#FF6F61", color: "black" }}
+                _focus={{ borderColor: "#FF3D00" }}
+            >
+                <option value="all">Todos los cursos</option>
+                <option value="Taller">Taller</option>
+                <option value="Tecnologia">Tecnología</option>
+                <option value="TICS">TICS</option>
+            </Select>
         </HStack>
     )
 }
