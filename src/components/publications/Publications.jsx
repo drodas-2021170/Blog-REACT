a4eb1d462e3709103ad40b9e3519a6f1069a9909
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-12 20:54:54 -0600

    Se modifica componente Publications

--- src/components/publications/Publications.jsx
+++ src/components/publications/Publications.jsx
@@ -1,23 +1,25 @@
-import { Flex, Heading, Stack } from "@chakra-ui/react"
+import { Box, Flex, Grid, Heading, Stack } from "@chakra-ui/react"
 import { CardPublication } from "./CardPublication"
 import { useContextPubication } from "../../shared/hooks/useContextPublication"
 import { useNavigate } from "react-router-dom"
 
-export const Publications = () => {
+export const Publications = ({publications}) => {
     let navigate = useNavigate()
-
-    const {publications} = useContextPubication()
     return (
-        <Flex alignItems='center' justifyContent='center' flexDirection='column'>
-            <Stack  spacing={50} padding={4}>
-                <div>
-                    <Heading>Publicaciones</Heading>
-                </div>
-                {console.log('Publications',publications)}
-                {publications.map((publi)=>(<CardPublication key={publi._id}
-                title={publi.title} description={publi.description} course={publi.course.name}
-                proyect={publi.proyect} creationDate={publi.creationDate} HandleClick={()=>{navigate(`/comments/${publi._id}`)}}/>))}
-            </Stack>
-        </Flex>
+        <Box padding="20px">
+            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+                {publications.map((publi) => (
+                    <CardPublication 
+                        key={publi._id} 
+                        title={publi.title} 
+                        description={publi.description} 
+                        proyect={publi.proyect} 
+                        course={publi.course.name} 
+                        creationDate={publi.creationDate} 
+                        HandleClick={() => navigate(`/comments/${publi._id}`)} 
+                    />
+                ))}
+            </Grid>
+        </Box>
     )
 }
