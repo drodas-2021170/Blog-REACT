e028ea54eceefb53d9c418086d120bb7bdc57837
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-12 20:54:32 -0600

    Se modifica componente Comments

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,30 +1,42 @@
 import { useParams } from "react-router-dom"
 import { useCommentsByPublication } from "../../shared/hooks/useCommentsByPublication"
-import { useEffect } from "react"
+import {  useContext, useEffect, useState } from "react"
 import { FadeLoader } from "react-spinners"
 import { CardPublication } from "../publications/CardPublication"
+import { Button } from "@chakra-ui/react"
+import { InitialFocus } from "../Modal/Modal"
+import { useComment } from "../../shared/hooks/useComment"
+
 
 export const Comments = () => {
     const {comments, isFetching, getCommentsByPublication} = useCommentsByPublication()
+    const {addComment,commentCount,setCommentCount} = useComment() 
 
+    
+    const [isModalOpen, setIsModalOpen] = useState(false)
     const {id} = useParams()
 
     useEffect(()=>{
             getCommentsByPublication(id)
-            console.log(id)
-            console.log('Comments',comments)
     },[])
 
+    
     if( isFetching){
             return(
                 <FadeLoader color='green'/>
             )
         }
 
+    const handleOpenModal = () => {
+    setIsModalOpen(!isModalOpen);
+    };
+
 
     return (
         <div>
-            {comments.map((comme)=>(<CardPublication key={comments._id} title={comme.author} description={comme.content} creationDate={comme.creationDate}/>))}
-        </div>
+            <Button colorScheme='teal' variant='solid' onClick={handleOpenModal}>Acciones</Button>
+            {isModalOpen && <InitialFocus />} 
+            {comments.map((comme)=>(<CardPublication key={comme._id} title={comme.author} description={comme.content} creationDate={comme.creationDate}/>))}
+    </div>
     )
 }
