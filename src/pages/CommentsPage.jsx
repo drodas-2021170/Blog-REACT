f3e1ed60de4d475fbd02bb8a857815e848b66968
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-14 18:21:15 -0600

    Se modifica Pagina Comments Page

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Comments } from '../components/comments/Comments'
+import { PublicationProvider } from '../context/PublicationContext'
 
 
 
 export const CommentsPage = () => {
     return (
         <>  
-           
+    <PublicationProvider>
+
                 <Comments/>
- 
+    </PublicationProvider>
+
         </>
     )
 }
