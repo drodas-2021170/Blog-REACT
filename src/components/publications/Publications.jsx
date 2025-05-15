b8ac7c84ee2a2daea38b1a0430bc2b29c97c5711
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-14 18:19:59 -0600

    Se modifica el componente Publications

--- src/components/publications/Publications.jsx
+++ src/components/publications/Publications.jsx
@@ -1,22 +1,22 @@
 import { Box, Flex, Grid, Heading, Stack } from "@chakra-ui/react"
 import { CardPublication } from "./CardPublication"
-import { useContextPubication } from "../../shared/hooks/useContextPublication"
 import { useNavigate } from "react-router-dom"
 
 export const Publications = ({publications}) => {
     let navigate = useNavigate()
     return (
-        <Box padding="20px">
+        <Box padding="20px"  margin="20px" backgroundColor="white" boxShadow="lg" rounded="lg">
             <Grid templateColumns="repeat(3, 1fr)" gap={6}>
                 {publications.map((publi) => (
                     <CardPublication 
+                    boxi={publi._id}
                         key={publi._id} 
                         title={publi.title} 
                         description={publi.description} 
                         proyect={publi.proyect} 
                         course={publi.course} 
                         creationDate={publi.creationDate} 
-                        HandleClick={() => navigate(`/comments/${publi._id}`)} 
+                        HandleClick={() => navigate(`/comments/${publi._id}`)}
                     />
                 ))}
             </Grid>
